eccdde4f74824eb3485e49672472415557b49570
sifat-jaber <sifatjaber@gmail.com>
2022-07-06 14:52:20 +0600

    Redesigned Let's Talk Section

--- components/LetsTalkSection.vue
+++ components/LetsTalkSection.vue
@@ -1,18 +1,18 @@
 <template>
 
-    <!-- Section 5 -->
+    <!-- Section 5
 
     <section>
         <div class="mx-20 mb-5 py-5">
             <div class="lg:grid lg:grid-cols-3">
 
-                <!-- Column 1 -->
+                Column 1
 
                 <div class="h-auto mx-auto lg:w-3/6 lg:min-w-1/3 lg:justify-self-center md:w-1/3 md:mb-20 md:justify-self-center sm:w-2/3 sm:justify-self-center sm:mb-20 ">
                     <img src="../assets/book-free-call-illustration-left.png" alt="">
                 </div>
 
-                <!-- Column 2 -->
+                Column 2
 
                 <div>
                     <h2 class="text-center h-auto w-full">
@@ -26,13 +26,36 @@
                     </h2>
                 </div>
 
-                <!-- Column 3 -->
+                Column 3
 
                 <div class="h-auto mx-auto lg:w-3/6 lg:min-w-1/3 lg:justify-self-center lg:mt-0 md:w-1/3 md:mt-20 md:justify-self-center sm:w-2/3 sm:justify-self-center sm:mt-20">
                     <img class="my-auto" src="../assets/book-free-call-illustration-right.png" alt="">
                 </div>
             </div>
         </div>
+    </section> -->
+
+
+    <section>
+
+        <div class="bg-[url('~/assets/world-map.png')] bg-fixed bg-center bg-contain bg-no-repeat h-full w-full lg:w-2/3 md:w-5/6 sm:w-5/6 mx-auto">
+
+            <div class="bg-white bg-opacity-90 h-full w-full py-32">
+
+                <h2 class="text-center w-fit mx-auto">
+
+                    <span class="block lg:text-2xl md:text-2xl sm:text-xl font-bold mb-10">Let's Start A Project</span>
+
+                    <span class="block lg:text-5xl md:text-5xl sm:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 hover:scale-110 duration-300 mb-10"><NuxtLink to="/Contact">Book a Free Call</NuxtLink></span>
+
+                    <span class="block lg:text-2xl md:text-2xl sm:text-xl font-bold">You Can Call Directly at (301) 244 0147</span>
+
+                </h2>
+
+            </div>
+
+        </div>
+
     </section>
 
 </template>
